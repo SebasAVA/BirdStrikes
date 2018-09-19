fbe024959dbd9964850890a6c8c2cc63f1b648db
Sebastian Rojas <js.rojas551@gmail.com>
2018-09-19 17:08:04 -0500

    Ya sube a la base de datos

--- src/renderer/ipcRendererEvents.js
+++ src/renderer/ipcRendererEvents.js
@@ -24,146 +24,87 @@ function setIpc(){
     console.log(allFiles);
   })
 }
-//
-//
-// function clearFile(){
-//   const oldFile = document.querySelectorAll('li.list-group-item')
-//   for (let i = 0; i < oldFile.length; i++) {
-//     oldFile[i].parentNode.removeChild(oldFile[i])
-//   }
-// }
-//
-// function cleaning(files, folderPaths)
-// {
-//   console.log(files);
-//    var rimraf = require('rimraf');
-//    var FinalFiles = [];
-//    Unidad = folderPaths[0];
-//   //
-//   for (var i = 0; i < files.length; i++) {
-//     if (files[i].filename.endsWith("QAR")||files[i].filename.endsWith("REC")||files[i].filename.endsWith("REP")||
-//         files[i].filename.endsWith("DAR")||files[i].filename.endsWith("MSG")||files[i].filename.endsWith("QA2")||
-//         files[i].filename.endsWith("ECT")||files[i].filename.endsWith("HCP")) {
-//         FinalFiles.push({filename: files[i].filesname, src: files[i].src});
-//     }
-//     else {
-//       if (fs.lstatSync(files[i].src).isFile()) {
-//          deleteFile(files[i])
-//       }
-//       else if (fs.lstatSync(files[i].src).isDirectory()) {
-//          deleteDir(files[i].src)
-//       }
-//       }
-//
-// }
-// }
-//Importante SERVIRA PARA ACTUALIZAR LAS MATRICULAS Y Saber el  nombre de los archivos finales
-// var FinalFiles = fs.readdirSync(folderPaths[0]);
-// console.log(FinalFiles);
-
-
-
-// function ACTail(files){
-// // Reiniciar una division para que quede en blanco
-//   const FilesList = document.querySelector('#ACTail')
-//   FilesList.innerHTML = '';
-//   console.log(files)
-//   let posMSG
-//
-//   for (var i = 0; i < files.length; i++) {
-//     var name = files[i].filename
-//     if(name.startsWith("_") && (name.endsWith(".QAR")||name.endsWith(".REC")||name.endsWith(".REP") ))
-//     {
-//           A32S = true
-//           fixACTail(Unidad)
-//           break;
-//     }
-//     else if (name.startsWith("MSG")) {
-//           posMSG = i
-//           A330 = true
-//     }
-//   }
-//   if (A32S == true) {
-//     if (name.substring(1,7).includes("-")) {
-//       console.log("Tiene GUION");
-//       console.log(name.substring(1, 7).replace('-',''));
-//       ACTAIL = name.substring(1, 7).replace('-','') ;
-//     }
-//     else{
-//       console.log("No tiene Guion");
-//     ACTAIL = name.substring(1, 7);}
-//     const node = `<h1><b>Matricula</b></h1>
-//                   <h2 id="AC">${name.substring(1, 7)}</h2>`
-//     FilesList.insertAdjacentHTML('beforeend', node)
-//     console.log(name.substring(1, 7));
-//   }
-//   if (A330 == true) {
-//     console.log(files[posMSG].src)
-//     let MSG = fs.readFile('G:/MSG.DAT')
-//     console.log(MSG);
-//   }
-// }
-//
-// function loadData (files){
-//   const FilesList = document.querySelector('ul.list-group')
-//                 for (let i = 0; i < files.length; i++) {
-//                   if (files[i].filename.endsWith("QAR")||files[i].filename.endsWith("REC")||files[i].filename.endsWith("REP")||
-//                       files[i].filename.endsWith("DAR")||files[i].filename.endsWith("MSG")||files[i].filename.endsWith("QA2")||
-//                       files[i].filename.endsWith("ECT")||files[i].filename.endsWith("HCP") ) {
-//                   const node = `<li class="list-group-item">
-//                                   <p>${files[i].src}</p>
-//                                   <div class="media-body">
-//                                     <strong>${files[i].filename}</strong>
-//                                     <p>${files[i].size}</p>
-//                                   </div>
-//                                 </li>`
-//                   FilesList.insertAdjacentHTML('beforeend', node)
-//                 }
-//                 }
-//     let ACFiles = []
-//     ACFiles = readPathU(Unidad);
-//     ACTail(ACFiles)
-//     Archivos = files;
-// }
 
 
 
 function uploadData()
 {
-  // Add the credentials to access your database
-var connection = mysql.createConnection({
-    host     : 'avianca.database.windows.net',
-    user     : 'etl_fda',
-    password : 'Avianca2017..', // or the original password : 'apaswword'
-    database : 'SEGOP'
-});
+  var Formulario = document.getElementById("FormBS").elements;
 
-// connect to mysql
-connection.connect(function(err) {
-    // in case of error
-    if(err){
-        console.log(err.code);
-        console.log(err.fatal);
-    }
-});
+  let NumOcu = Formulario[0].value;
+  let LugOcu = Formulario[2].value;
+  let NumAvesGol = Formulario[15].value;
+  console.log(Formulario);
 
-// Perform a query
-var query = 'SELECT TOP (1000) * FROM [dbo].[Birdstrikes]';
+  var Connection = require('tedious').Connection;
+  var resultQ = []
+  var config = {
+      userName: 'etl_fda',
+      password: 'Avianca2017..',
+      server: 'avianca.database.windows.net',
+      // If you are on Microsoft Azure, you need this:
+      options: {encrypt: true, database: 'SEGOP'}
+  }
 
-connection.query(query, function(err, rows, fields) {
-    if(err){
-        console.log("An error ocurred performing the query.");
-        console.log(err);
-        return;
-    }
+  var connection = new Connection(config);
+     connection.on('connect', function(err) {
+         // If no error, then good to proceed.
+         console.log("Connected");
+         executeStatement();
+     });
 
-    console.log("Query succesfully executed", rows);
-});
+     var Request = require('tedious').Request;
+     var TYPES = require('tedious').TYPES;
 
-// Close the connection
-connection.end(function(){
-    // The connection has been closed
-});
+     function executeStatement() {
+         var request = new Request(`INSERT INTO [dbo].[Birdstrikes]
+           ([Ocurrencia]
+           ,[NumAves]
+           ,[Base])
+     VALUES
+           ('${NumOcu}'
+           ,12
+           ,'${LugOcu}')`, function(err) {
+         if (err) {
+             console.log(err);}
+         });
+         var result = "";
+         request.on('row', function(columns) {
+           console.log(columns);
+           // const notamQ = document.querySelector('.image-container')
+           // const node = `
+           // <div class="notam-container">
+           // <span class="nav-group-item">
+           //  <span class="icon icon-flight"></span>
+           //   <h4>Consecutivo: </h4><b> ${columns[0].value}</b>
+           //
+           //  </span>
+           //  <h4>${columns[1].value}</h4>
+           //  <p>${columns[2].value}</p>
+           //  <p>Vigente desde: ${columns[3].value} Hasta: ${columns[4].value}</p>
+           // <p>${columns[6].value}</p>
+           // </div>
+           //  `
+             //  notamQ.insertAdjacentHTML('beforeend', node)
+             // columns.forEach(function(column) {
+             //   if (column.value === null) {
+             //     // console.log('NULL');
+             //   } else {
+             //     result+= column.value + "|";
+             //   }
+             // });
+             //   console.log(result);
+
+             // resultQ.push(result)
+             // result ="";
+         });
+
+         request.on('done', function(rowCount, more) {
+         console.log(rowCount + ' rows returned');
+         });
+         connection.execSql(request);
+     }
+
 }
 
 
